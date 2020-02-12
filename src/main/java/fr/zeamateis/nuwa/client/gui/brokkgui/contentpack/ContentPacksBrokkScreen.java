1a914a9541597437007d70b2fc279c04defa24cd
ZeAmateis <theamateis@gmail.com>
2020-02-12 18:46:50 +0100

    + Seperate panels + Start working on itemstack render panel

--- src/main/java/fr/zeamateis/nuwa/client/gui/brokkgui/contentpack/ContentPacksBrokkScreen.java
+++ src/main/java/fr/zeamateis/nuwa/client/gui/brokkgui/contentpack/ContentPacksBrokkScreen.java
@@ -1,30 +1,40 @@
 package fr.zeamateis.nuwa.client.gui.brokkgui.contentpack;
 
-import api.contentpack.ContentPack;
 import fr.zeamateis.nuwa.NuwaMod;
+import fr.zeamateis.nuwa.contentpack.common.minecraft.blocks.base.IJsonBlock;
+import fr.zeamateis.nuwa.contentpack.common.minecraft.items.base.IJsonItem;
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.SimpleSound;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.SoundEvents;
-import net.voxelindustry.brokkgui.animation.transition.FadeTransition;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.voxelindustry.brokkgui.data.RectAlignment;
 import net.voxelindustry.brokkgui.data.RectBox;
+import net.voxelindustry.brokkgui.data.RelativeBindingHelper;
 import net.voxelindustry.brokkgui.element.GuiLabel;
 import net.voxelindustry.brokkgui.element.GuiListView;
 import net.voxelindustry.brokkgui.gui.BrokkGuiScreen;
 import net.voxelindustry.brokkgui.paint.Color;
+import net.voxelindustry.brokkgui.panel.GuiAbsolutePane;
 import net.voxelindustry.brokkgui.panel.GuiRelativePane;
 import net.voxelindustry.brokkgui.shape.Rectangle;
+import net.voxelindustry.brokkgui.wrapper.elements.ItemStackView;
 import org.apache.commons.io.FileUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ContentPacksBrokkScreen extends BrokkGuiScreen {
 
     private final Minecraft minecraft;
     private final PackListPane packListPane;
+    private final PackInfoPane packInfoPane;
+    private final ContentPackObjectPane contentPackObjectPane;
 
     public ContentPacksBrokkScreen(Minecraft minecraft) {
         super(0.5f, 0.5f, Minecraft.getInstance().mainWindow.getScaledWidth(), Minecraft.getInstance().mainWindow.getScaledHeight());
@@ -37,6 +47,12 @@ public class ContentPacksBrokkScreen extends BrokkGuiScreen {
         this.packListPane = new PackListPane();
         mainPane.addChild(this.packListPane, 0.22f, 0.5f);
 
+        this.packInfoPane = new PackInfoPane();
+        mainPane.addChild(this.packInfoPane, 0.8f, 0.5f);
+
+        contentPackObjectPane = new ContentPackObjectPane();
+        mainPane.addChild(this.contentPackObjectPane, 0.8f, 0.8f);
+
         this.addStylesheet("/assets/nuwa/screen/css/demo.css");
     }
 
@@ -45,21 +61,67 @@ public class ContentPacksBrokkScreen extends BrokkGuiScreen {
         super.initGui();
     }
 
+    static class ContentPackObjectPane extends GuiRelativePane {
+
+        private List<Item> objects = new ArrayList<>();
+
+        public ContentPackObjectPane() {
+            this.setBackgroundColor(Color.GRAY);
+            this.setSize(128, 128);
+
+            GuiAbsolutePane mainPanel = new GuiAbsolutePane();
+            mainPanel.setWidthRatio(1);
+            mainPanel.setHeightRatio(1);
+            this.addChild(mainPanel);
+            RelativeBindingHelper.bindToPos(mainPanel, this);
+
+
+            List<Item> items = ForgeRegistries.BLOCKS.getEntries()
+                    .stream()
+                    .map(Map.Entry::getValue)
+                    .filter(block -> block instanceof IJsonBlock)
+                    //.filter(block -> block.getRegistryName().getNamespace().equals(contentPackNamespace))
+                    .map(Block::asItem).collect(Collectors.toList());
+
+            items.addAll(ForgeRegistries.ITEMS.getEntries().stream()
+                    .map(Map.Entry::getValue)
+                    .filter(item -> item instanceof IJsonItem)
+                    //.filter(block -> block.getRegistryName().getNamespace().equals(contentPackNamespace))
+                    .map(Item::asItem).collect(Collectors.toList()));
+
+            System.out.println(items.size());
+
+            for (int i = 0; i < items.size(); i++)
+                for (int j = 0; j < 3; ++j) {
+                    for (int k = 0; k < 7; ++k) {
+                        ItemStackView blockView = new ItemStackView(new ItemStack(items.get(i), 1));
+                        blockView.setItemTooltip(true);
+                        blockView.setWidth(18);
+                        blockView.setHeight(18);
+                        blockView.setzLevel(301);
+                        mainPanel.addChild(blockView, 8 + k * 18, 18 + j * 18);
+                    }
+                }
+
+
+            /*if (!objects.isEmpty())
+                for (int x = 0; x < objects.size(); x++) {
+                    ItemStackView blockView = new ItemStackView(new ItemStack(objects.get(x), 1));
+                    blockView.setItemTooltip(true);
+                    blockView.setWidth(18);
+                    blockView.setHeight(18);
+                    this.addChild(blockView, 18 * x, 18);
+                }*/
+        }
+    }
+
     class PackListPane extends GuiRelativePane {
         public PackListPane() {
             this.setWidth(170);
             this.setHeight(160);
             this.setBackgroundColor(Color.LIGHT_GRAY);
 
-            final GuiListView<GuiRelativePane> contentPacksPane = new GuiListView<>();
-            AnimatedInfoPane infoPane = new AnimatedInfoPane();
-            infoPane.setVisible(false);
-
-            FadeTransition transition = new FadeTransition(infoPane, 2, TimeUnit.SECONDS);
-            transition.setMaxCycles(1);
-            transition.setReverse(false);
-            transition.setStartOpacity(0);
-            transition.setEndOpacity(1.0);
+            final GuiListView<GuiAbsolutePane> contentPacksPane = new GuiListView<>();
 
             int cellHeight = 32;
 
@@ -68,12 +130,12 @@ public class ContentPacksBrokkScreen extends BrokkGuiScreen {
             contentPacksPane.setCellHeight(cellHeight);
             contentPacksPane.setCellWidth(getWidth() - 2);
 
-            List<GuiRelativePane> packInfoPanes = new ArrayList<>();
+            List<GuiAbsolutePane> packInfoPanes = new ArrayList<>();
 
             NuwaMod.getPackManager().getPacks().forEach(contentPack -> {
-                GuiRelativePane packInfoPane = new GuiRelativePane();
-                packInfoPane.setBackgroundColor(Color.fromHex("#666666"));
-                packInfoPane.setWidth(getWidth());
+                GuiAbsolutePane packEntryPane = new GuiAbsolutePane();
+                packEntryPane.setBackgroundColor(Color.fromHex("#666666"));
+                packEntryPane.setWidth(getWidth());
 
                 Rectangle packIcon = new Rectangle();
                 if (contentPack.getPackIcon() != null) {
@@ -81,84 +143,63 @@ public class ContentPacksBrokkScreen extends BrokkGuiScreen {
                     packIcon.setStyle("background-texture: assets(\"" + Minecraft.getInstance().getTextureManager().getDynamicTextureLocation("contentpackicon", new DynamicTexture(contentPack.getPackIcon())).toString() + "\", 32px, 32px)");
                     packIcon.setyTranslate(1);
                     packIcon.setxTranslate(1);
-                    packInfoPane.addChild(packIcon, 0.25F, 0.2F);
+                    packEntryPane.addChild(packIcon, 0.25F, 0.2F);
                 }
 
+                //Pack Name
                 GuiLabel packName = new GuiLabel(contentPack.getPackName());
                 packName.setWidth(getWidth() - 2);
                 packName.setTextAlignment(RectAlignment.LEFT_CENTER);
                 packName.setTextPadding(new RectBox(10, contentPack.getPackIcon() != null ? packIcon.getLeftPos() + 32 : 2, 2, 0));
 
+                //Pack Version
                 GuiLabel packVersion = new GuiLabel(contentPack.getVersion());
                 packVersion.setWidth(getWidth() - 2);
                 packVersion.setTextAlignment(RectAlignment.LEFT_CENTER);
                 packVersion.setTextPadding(new RectBox(20, contentPack.getPackIcon() != null ? packIcon.getLeftPos() + 32 : 2, 2, 0));
 
+                //Content Pack File Size
                 GuiLabel packFileSize = new GuiLabel(FileUtils.byteCountToDisplaySize(contentPack.getZipFileSize()));
                 packFileSize.setWidth(getWidth() - 2);
                 packFileSize.setTextAlignment(RectAlignment.RIGHT_CENTER);
                 packFileSize.setTextPadding(new RectBox(30, contentPack.getPackIcon() != null ? packIcon.getLeftPos() + 32 : 2, 2, 2));
 
-                packInfoPane.addChilds(packName, packVersion, packFileSize);
+                packEntryPane.addChilds(packName, packVersion, packFileSize);
 
-
-                packInfoPane.setOnClickEvent(event -> {
+                packEntryPane.setOnClickEvent(event -> {
                     minecraft.getSoundHandler().play(SimpleSound.master(SoundEvents.UI_BUTTON_CLICK, 1.0F));
-                    System.out.println(contentPack.getPackName());
-                    infoPane.setContentPack(contentPack);
-
-                    if (infoPane.getChildrens() != null) {
-                        infoPane.getChildrens().clear();
-                    } else {
-                        infoPane.addChild(packName);
-                    }
-
-                    infoPane.setVisible(true);
-
-                    if (transition.isRunning())
-                        transition.reset();
-                    else {
-                        transition.restart();
-                    }
+                    packInfoPane.packName.setText(contentPack.getPackName());
+                    packInfoPane.contentPackNamespace = contentPack.getNamespace();
                 });
 
-                packInfoPanes.add(packInfoPane);
+                packInfoPanes.add(packEntryPane);
             });
 
             contentPacksPane.setElements(packInfoPanes);
             contentPacksPane.setCellYPadding(2);
 
             this.addChild(contentPacksPane);
-            this.addChild(infoPane, 1.8f, 0.5f);
-
         }
     }
 
-    class AnimatedInfoPane extends GuiRelativePane {
+    class PackInfoPane extends GuiRelativePane {
 
-        private ContentPack contentPack;
+        GuiLabel packName = new GuiLabel();
+        private String contentPackNamespace;
 
-        public AnimatedInfoPane() {
+        public PackInfoPane() {
             this.setWidth(170);
             this.setHeight(160);
             this.setBackgroundColor(Color.GREEN);
 
-            if (this.contentPack != null) {
-                GuiLabel packName = new GuiLabel(contentPack.getPackName());
-                packName.setWidth(getWidth() - 2);
-                packName.setTextAlignment(RectAlignment.LEFT_CENTER);
-                packName.setTextPadding(new RectBox(10, 2, 2, 0));
+            packName.setWidth(getWidth() - 2);
+            packName.setTextAlignment(RectAlignment.LEFT_CENTER);
+            packName.setTextPadding(new RectBox(10, 2, 2, 0));
 
-                this.addChild(packName);
-            }
-        }
+            this.addChild(packName);
 
-        public ContentPack getContentPack() {
-            return contentPack;
-        }
-
-        public void setContentPack(ContentPack contentPack) {
-            this.contentPack = contentPack;
+            //SubWindow subWindow = new SubWindow(items);
+            //this.addChild(subWindow);
         }
     }
 }
